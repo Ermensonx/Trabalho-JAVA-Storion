efc5e4656cafed4ec4e57ad169a4ef99e9913111
Ermenson <99372234+Ermensonx@users.noreply.github.com>
2024-09-23 15:15:25 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Criando o cliente (sem passar saldo inicial)
         Cliente c1 = new Cliente();
+        
+        // O cliente faz o cadastro e depois entra no menu principal
         c1.cadastrar();
         c1.comecar();
     }
